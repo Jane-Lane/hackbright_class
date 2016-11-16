b979341e1f97190ac10912237b59141ae56cf2cf
Elizabeth Goodman <elizabethgoodman@Elizabeths-MacBook.local>
2016-11-16 15:32:04 -0800

    made stars concentric but turned

--- turtle_art.py
+++ turtle_art.py
@@ -1,4 +1,6 @@
 import turtle
+import math
+
 window = turtle.Screen()
 window.bgcolor("black")
 
@@ -6,24 +8,51 @@ def make_star (new_turtle):
     new_turtle.pendown()
     for x in range(5):
         new_turtle.forward(200)
-        new_turtle.left(36)
+        new_turtle.left(18) #Makes the stamp face directly outward.
         new_turtle.stamp()
-        new_turtle.left(180)
-    new_turtle.penup
+        new_turtle.left(198)
+    new_turtle.penup()
+
+def circumscribe(new_turtle):
+    new_turtle.pendown()
+    radius = 100/math.cos(math.pi/10) #radius from center of star to its tip.
+    new_turtle.right(108)
+    new_turtle.circle(radius)
+    new_turtle.left(108)
+    new_turtle.penup()
 
 
-sarita = turtle.Turtle()
-sarita.shape('turtle')
-sarita.color('yellow')
-sarita.speed('slowest')
+def displace(new_turtle):
+    new_turtle.penup()
+    radius = 100/math.cos(math.pi/10) #radius from center of star to its tip.
+    new_turtle.right(108) #Faces perpendicular to directly outward.
+    new_turtle.circle(radius, 18)
+    new_turtle.left(108)
+    new_turtle.pendown()
 
-make_star(sarita)
+def main():
+    sarita = turtle.Turtle()
+    sarita.shape('turtle')
+    sarita.color('yellow')
+    sarita.speed('slow')
 
-sarita.color('blue')
-make_star(sarita)
-sarita.color('purple')
-make_star(sarita)
-sarita.color('red')
-make_star(sarita)
+    make_star(sarita)
+    displace(sarita)
+    sarita.color('blue')
+    make_star(sarita)
+    displace(sarita)
+    sarita.color('purple')
+    make_star(sarita)
+    displace(sarita)
+    sarita.color('red')
+    make_star(sarita)
+
+    #To make a circle through all the points:
+    #sarita.color('white')
+    #circumscribe(sarita)
+
+if __name__ == "__main__":
+    main()
+
 
 window.exitonclick()
